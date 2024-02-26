0a0708d0a051fa4489de6343b981e55f72c1a0b0
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2024-02-27 01:02:23 +0600

    [feat] [refactor] Changes photo and appointment_letter fields, refactor the user name

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'John Doe',
+            'name' => 'Rakibur Rahman Rimon',
             'email' => 'rakiburrimon@gmail.com',
             'password' => Hash::make('password'),
         ]);
